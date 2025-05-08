9732e7690240c8c6077786dc373dd39549acef86
MuhammadMaazuddin <maaz68926@gmail.com>
2025-05-08 23:44:05 +0500

    new file:   app/components/anim.tsx 	modified:   app/components/navbar.tsx 	modified:   app/components/section_1.tsx 	modified:   app/components/section_2.tsx 	modified:   app/globals.css 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   public/cheetha.png

--- app/components/section_1.tsx
+++ app/components/section_1.tsx
@@ -2,7 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import TypingEffect from "./anim";
 
 export default function Section1() {
  
@@ -14,23 +16,23 @@ export default function Section1() {
       <div className="max-w-7xl mx-auto px-0 grid grid-cols-1 md:grid-cols-2 items-center gap-80">
         {/* Left: Intro Text */}
         <div className="text-left">
-          <h1 className="text-5xl font-bold mb-4 leading-tight">
-            Welcome to <span className="text-yellow-400">Cheetha</span>
+          <h1 className="text-5xl font-bold mb-6 leading-tight">
+            Welcome to <span className="text-purple-400">Cheetha</span>
           </h1>
-          <h2 className="text-3xl mb-2 leading-tight font-semibold">
-            Product Developer
-          </h2>
-          <p className="text-md italic text-yellow-500 mb-4">
+          <p className="mb-4">
+            <TypingEffect />
+          </p>
+          <p className="text-md italic text-purple-500 mb-4">
             Built for the fastest minds.
           </p>
           <p className="text-xl text-foreground font-light mb-6">
             All-in-one developer toolkit combining the power of AI, Blockchain, and productivity tools — designed for speed, simplicity, and scalability.
           </p>
-          <Button
-            className="bg-yellow-400 hover:bg-yellow-600 text-yellow-950 text-sm px-6 py-3 "
-          >
-            Get Started
-          </Button>
+          <Link href="/app">
+            <Button asChild>
+              <span>Get Started</span>
+            </Button>
+          </Link>
         </div>
 
         {/* Right: Image */}
